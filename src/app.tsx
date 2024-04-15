a6f871e2a404191a7f5e8dd29d352c1178bd1cde
Faustino Zanetto <zanettofaustino@gmail.com>
2024-04-15 11:32:47 -0300

    feat(core): updated deps and started working on base components

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div className="grid place-content-center min-h-screen bg-white relative"></div>;
+  return (
+    <div className="flex flex-col items-center px-4 my-4">
+      <h1 className="text-3xl font-extrabold text-center md:text-4xl">Truth Table Generator</h1>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/modules/theme/components/theme-toggler.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import React from 'react';
+import { useTheme } from 'next-theme-kit';
+import { ButtonProps, Button } from 'ui/components/button';
+
+interface ThemeTogglerProps extends Omit<ButtonProps, 'onClick'> {
+  children?: React.ReactNode;
+}
+
+const ThemeToggler: React.FC<ThemeTogglerProps> = (props) => {
+  const { children, ...rest } = props;
+  const { theme, setTheme } = useTheme();
+
+  const handleThemeChange = (): void => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  return (
+    <Button aria-label="Toggle Theme" onClick={handleThemeChange} {...rest}>
+      <svg
+        className="block h-6 w-6 stroke-current dark:hidden"
+        fill="none"
+        height="35"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+        width="35"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+        <circle cx="12" cy="12" r="4" />
+        <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
+      </svg>
+      <svg
+        className="hidden h-6 w-6 stroke-current dark:block"
+        fill="none"
+        height="35"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+        width="35"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M0 0h24v24H0z" fill="none" stroke="none" />
+        <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+      </svg>
+      {children}
+    </Button>
+  );
+};
+
+export default ThemeToggler;
